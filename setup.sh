212da6107d36713e782e92495595d2613675f6fd
Philipp Müller <philipp@phepranto.dev>
2021-07-08 19:08:29 +0200

    Fixed symbolic linking in setup.sh

--- setup.sh
+++ setup.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 cd ~/dotfiles
-ln -s .zshrc ~/.zshrc
-ln -s sway ~/.config/
-ln -s wayfire.ini ~/.config/
-ln -s wf-shell.ini ~/.config/
+ln -s ~/dotfiles/.zshrc ~/.zshrc
+ln -s ~/dotfiles/sway ~/.config/
+ln -s ~/dotfiles/wayfire.ini ~/.config/
+ln -s ~/dotfiles/wf-shell.ini ~/.config/
