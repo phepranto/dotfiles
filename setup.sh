d91bb72e9498b873f718a1bd8531601b1cef4cb7
Philipp Müller <philipp@phepranto.dev>
2021-07-09 18:25:01 +0200

    Swaysome > setup.sh

--- setup.sh
+++ setup.sh
@@ -6,3 +6,4 @@ ln -s ~/dotfiles/sway ~/.config/
 ln -s ~/dotfiles/wayfire.ini ~/.config/
 ln -s ~/dotfiles/wf-shell.ini ~/.config/
 ln -s ~/dotfiles/pulse/default.pa ~/.config/pulse/
+ln -s ~/dotfiles/sway/config.d ~/.config/sway/
