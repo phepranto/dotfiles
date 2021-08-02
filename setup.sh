15b5b91d32b5fd76276ee7a72b10961dace65d52
Philipp Mueller <philipp@phepranto.dev>
2021-08-02 12:05:09 +0200

    Removed wayfire config

--- setup.sh
+++ setup.sh
@@ -4,8 +4,6 @@ cd ~/dotfiles
 ln -s --force ~/dotfiles/.zshrc ~/.zshrc
 ln -s --force ~/dotfiles/.zsh_aliases ~/.zsh_aliases
 ln -s --force ~/dotfiles/sway ~/.config/
-ln -s --force ~/dotfiles/wayfire.ini ~/.config/
-ln -s --force ~/dotfiles/wf-shell.ini ~/.config/
 ln -s --force ~/dotfiles/pulse/default.pa ~/.config/pulse/
 mkdir ~/.config/waybar/
 ln -s --force ~/dotfiles/waybar/config ~/.config/waybar/config
